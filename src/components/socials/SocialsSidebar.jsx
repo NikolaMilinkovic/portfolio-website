20dbcdc721db57bd4093544427042ac8497ebb86
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-07-13 21:20:35 +0200

    Test icon path rendering for vercel

--- src/components/socials/SocialsSidebar.jsx
+++ src/components/socials/SocialsSidebar.jsx
@@ -5,13 +5,13 @@ function SocialsSidebar() {
   return (
     <aside className="socials-sidebar">
       <a className="sidebar-icon-link" href="https://www.linkedin.com/in/nikola-milinkovic-2231451a6/" target="_blank">
-        <img className="sidebar-icon" alt="LinkedIn" src="/public/icons/black/linkedin.svg" />
+        <img className="sidebar-icon" alt="LinkedIn" src="/icons/black/linkedin.svg" />
       </a>
       <a className="sidebar-icon-link" href="https://github.com/NikolaMilinkovic" target="_blank">
-        <img className="sidebar-icon" alt="GitHub" src="/public/icons/black/square-github.svg" />
+        <img className="sidebar-icon" alt="GitHub" src="/icons/black/square-github.svg" />
       </a>
       <a className="sidebar-icon-link" href="https://www.instagram.com/nikola__milinkovic/" target="_blank">
-        <img className="sidebar-icon" alt="Instagram" src="/public/icons/black/square-instagram.svg" />
+        <img className="sidebar-icon" alt="Instagram" src="/icons/black/square-instagram.svg" />
       </a>
     </aside>
   );
