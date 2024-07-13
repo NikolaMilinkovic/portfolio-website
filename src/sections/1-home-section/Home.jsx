dc8d09204781e3c1e9a8ac71f9098316fe8c5f73
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-07-13 21:22:17 +0200

    Fix image path for Home.jsx

--- src/sections/1-home-section/Home.jsx
+++ src/sections/1-home-section/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <Element id="scroll-home" name="scroll-home">
       <section className="home-section">
         <SocialsSidebar />
-        <img className="test-image" alt="parallax-vector" src="public/images/image-4.png" />
+        <img className="test-image" alt="parallax-vector" src="/images/image-4.png" />
       </section>
     </Element>
 
