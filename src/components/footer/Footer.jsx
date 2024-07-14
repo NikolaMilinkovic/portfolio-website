6fe977519f3563b05304b7480f0abce32eef950d
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-07-15 00:46:26 +0200

    Add footer & about me styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,37 @@ import './Footer.scss';
 function Footer() {
   return (
     <footer>
-      <div>This is my footer</div>
+      <div className="upper-section">
+        <div className="left">
+          <div>
+            <p className="footer-header">Nikola Milinkovic</p>
+            <p className="footer-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis itaque vero est rem provident dignissimos.</p>
+          </div>
+        </div>
+
+        <div className="right">
+          <div className="footer-socials">
+            <p className="socials-text">socials</p>
+            <div>
+              <a className="footer-socials-icon-link" href="https://www.linkedin.com/in/nikola-milinkovic-2231451a6/" target="_blank">
+                <img className="footer-icon" alt="LinkedIn" src="/icons/white/linkedin-white.svg" />
+              </a>
+              <a className="footer-socials-icon-link" href="https://github.com/NikolaMilinkovic" target="_blank">
+                <img className="footer-icon" alt="GitHub" src="/icons/white/square-github-white.svg" />
+              </a>
+              <a className="footer-socials-icon-link" href="https://www.instagram.com/nikola__milinkovic/" target="_blank">
+                <img className="footer-icon" alt="Instagram" src="/icons/white/square-instagram-white.svg" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="lower-section">
+        <p className="copyright">
+          © Copyright 2024. Made by&nbsp;
+          <a href="https://github.com/NikolaMilinkovic/portfolio-website" target="_blank">Nikola Milinkovic</a>
+        </p>
+      </div>
     </footer>
   );
 }
--- src/sections/2-about-section/About.jsx
+++ src/sections/2-about-section/About.jsx
@@ -4,12 +4,27 @@ import './About.scss';
 
 function About() {
   return (
-    <section className="about-section">
+    <>
       <Element id="scroll-about" name="scroll-about" />
-      <p className="test-about-me">
-        My Expertise
-      </p>
-    </section>
+      <section className="about-section">
+        <div className="about-left">
+          <p className="about-section-hero-text">
+            About Me
+          </p>
+          <div className="border-div" />
+          <img alt="Nikola Milinkovic" src="/images/moja-slika.png" className="profile-image" />
+        </div>
+        <div className="about-right">
+          <h2 className="about-header">
+            Random inspiring header
+            <div className="header-underline" />
+          </h2>
+          <article className="about-article">
+            <p className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident sunt soluta veniam. Explicabo ipsum quis minima labore esse? Impedit quia laudantium excepturi nostrum tenetur necessitatibus eaque nisi debitis inventore illo.</p>
+          </article>
+        </div>
+      </section>
+    </>
   );
 }
 
