54c0ea3ea5cedfdda86c75cf0857a285532752f3
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-07-28 23:06:49 +0200

    Test AI page layout

--- src/sections/4-AI-section/AiSection.jsx
+++ src/sections/4-AI-section/AiSection.jsx
@@ -1,18 +1,32 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Element } from 'react-scroll';
 import './AiSection.scss';
 import ContactForm from '../../components/contactForm/ContactForm';
+import useElementOnScreen from '../../util/useElementOnScreen';
 
 function AiSection() {
+  const [isImgVisible, setIsImgVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const [containerRef] = useElementOnScreen({
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+  }, isVisible, setIsVisible, true);
   return (
     <Element id="scroll-ai" name="scroll-ai">
-      <section className="ai-section">
-        <div className="info-area">
-          <h1>Ask AI!</h1>
-          <p>Looking to find out more about me? Ask away! The AI companion will try his best to give you all the information that you need.</p>
+      <section className="ai-section" ref={containerRef}>
+        <div className="ai-left">
+          <h1 className={`about-header ${isVisible ? 'show-element-right' : 'hide-element-right'}`}>
+            Ask AI all you want to know about me!
+            <div className={`header-underline ${isVisible ? 'show-element-right' : 'hide-element-right'}`} />
+          </h1>
+          <div className="ai-section-hero-text">
+            <p>Looking to find out more about me? Ask away! The AI companion will try his best to give you all the information that you need.</p>
+          </div>
         </div>
-        <div className="chat-section">
+        <div className="ai-right">
           {/* <div className="chat-display">
             <p>This is the chat section</p>
           </div>
