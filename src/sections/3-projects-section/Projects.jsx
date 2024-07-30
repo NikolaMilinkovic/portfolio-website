12388aa98508d3a86b6edc263cccd119319c18b0
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-07-31 00:41:22 +0200

    add color fill to project 1

--- src/sections/3-projects-section/Projects.jsx
+++ src/sections/3-projects-section/Projects.jsx
@@ -84,6 +84,7 @@ function Projects() {
             <AnimatePr1 />
           </div>
           <div className="ground" />
+          <div className="color-fill" />
           <svg className="path-container" width="100%" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
             <path fill="#D9EAC0" fillOpacity="1" d="M0,160L80,160C160,160,320,160,480,138.7C640,117,800,75,960,53.3C1120,32,1280,32,1360,32L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z" />
           </svg>
