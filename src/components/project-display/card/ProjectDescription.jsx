16ea775cb1effc04a00a2f39f879aec67a413921
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-07-31 19:48:10 +0200

    Update fox section res design

--- src/components/project-display/card/ProjectDescription.jsx
+++ src/components/project-display/card/ProjectDescription.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import TechBox from './techBox/TechBox';
 import './ProjectDescription.scss';
 
-function ProjectDescription({ projectData, color, background }) {
+function ProjectDescription({ projectData, color, caseStudyPath = '#' }) {
   const [data, setData] = useState(null);
 
   useEffect(() => {
@@ -12,7 +12,13 @@ function ProjectDescription({ projectData, color, background }) {
 
   return (
     <div className="project-description">
-      <p className="project-text">{data && (data.landingText)}</p>
+      <p className="project-text">
+        {data && (data.landingText)}
+        {' '}
+        For more information see this projects
+        {' '}
+        <a className="case-study-link" href={caseStudyPath}>case study.</a>
+      </p>
 
       <div className="tech-used">
         {data && data.usedTech.map((tech, index) => (
