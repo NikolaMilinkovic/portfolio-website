6cbfc85ec332527401a254c02a7b8f65773a3f2d
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-08-01 22:20:42 +0200

    Test vercel string for case study path

--- src/sections/3-projects-section/Projects.jsx
+++ src/sections/3-projects-section/Projects.jsx
@@ -72,10 +72,10 @@ function Projects() {
         <section className="projects-section-1">
           <ProjectSidebar
             projectData={mcSchemMan || null}
-            caseStudyLink="https://nikola-portfolio-website.vercel.app/case-study/mc-schematic-manager"
+            caseStudyLink="http://nikola-portfolio-website.vercel.app/case-study/mc-schematic-manager"
           />
           <Project
-            caseStudyLink="https://nikola-portfolio-website.vercel.app/case-study/mc-schematic-manager"
+            caseStudyLink="http://nikola-portfolio-website.vercel.app/case-study/mc-schematic-manager"
             projectData={mcSchemMan || null}
             projectDescritpionColor="black"
             animateSide="right"
@@ -100,11 +100,11 @@ function Projects() {
         <section className="projects-section-2">
           <ProjectSidebar
             projectData={battleship || null}
-            caseStudyLink="https://nikola-portfolio-website.vercel.app/case-study/battleship"
+            caseStudyLink="http://nikola-portfolio-website.vercel.app/case-study/battleship"
             color="white"
           />
           <Project
-            caseStudyLink="https://nikola-portfolio-website.vercel.app/case-study/battleship"
+            caseStudyLink="http://nikola-portfolio-website.vercel.app/case-study/battleship"
             projectData={battleship || null}
             projectDescritpionColor="white"
             headerColor="white"
@@ -124,10 +124,10 @@ function Projects() {
           </div>
           <ProjectSidebar
             projectData={cliDatStruct || null}
-            caseStudyLink="https://nikola-portfolio-website.vercel.app/case-study/cli-data-structures"
+            caseStudyLink="http://nikola-portfolio-website.vercel.app/case-study/cli-data-structures"
           />
           <Project
-            caseStudyLink="https://nikola-portfolio-website.vercel.app/case-study/cli-data-structures"
+            caseStudyLink="http://nikola-portfolio-website.vercel.app/case-study/cli-data-structures"
             projectData={cliDatStruct || null}
             projectDescritpionColor="black"
             timeout={1000}
@@ -147,11 +147,11 @@ function Projects() {
         <section className="projects-section-4">
           <ProjectSidebar
             projectData={portfolioWebsite || null}
-            caseStudyLink="https://nikola-portfolio-website.vercel.app/case-study/portfolio-website"
+            caseStudyLink="http://nikola-portfolio-website.vercel.app/case-study/portfolio-website"
             color="white"
           />
           <Project
-            caseStudyLink="https://nikola-portfolio-website.vercel.app/case-study/portfolio-website"
+            caseStudyLink="http://nikola-portfolio-website.vercel.app/case-study/portfolio-website"
             projectData={portfolioWebsite || null}
             projectDescritpionColor="white"
             headerColor="white"
