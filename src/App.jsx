be503c18a7aee3a5f2a083e7923d0b6d10497893
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-08-01 22:48:16 +0200

    Test links

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
           {/* BATTLESHIP */}
           <Route
-            path="nikola-portfolio-website.vercel.app/case-study/battleship"
+            path="https://nikola-portfolio-website.vercel.app/case-study/battleship"
             element={(
               <ProjectPage
                 projectUrl="/files/projects/battleship.json"
@@ -36,7 +36,7 @@ function App() {
 
           {/* MC-SCHEM-MAN */}
           <Route
-            path="nikola-portfolio-website.vercel.app/case-study/mc-schematic-manager"
+            path="https://nikola-portfolio-website.vercel.app/case-study/mc-schematic-manager"
             element={(
               <ProjectPage
                 projectUrl="/files/projects/mc-schematic-manager.json"
@@ -46,7 +46,7 @@ function App() {
 
           {/* CLI DATA STRUCTURES */}
           <Route
-            path="nikola-portfolio-website.vercel.app/case-study/cli-data-structures"
+            path="https://nikola-portfolio-website.vercel.app/case-study/cli-data-structures"
             element={(
               <ProjectPage
                 projectUrl="/files/projects/cli-data-structures.json"
@@ -55,7 +55,7 @@ function App() {
           />
           {/* CLI DATA STRUCTURES */}
           <Route
-            path="nikola-portfolio-website.vercel.app/case-study/portfolio-website"
+            path="https://nikola-portfolio-website.vercel.app/case-study/portfolio-website"
             element={(
               <ProjectPage
                 projectUrl="/files/projects/portfolio-website.json"
