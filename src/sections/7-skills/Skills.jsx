c98f3da74d341d78c0219bf4b53ff421635de683
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-08-04 18:12:27 +0200

    Add skills section

--- src/sections/7-skills/Skills.jsx
+++ src/sections/7-skills/Skills.jsx
@@ -1,24 +1,72 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useEffect, useState, useRef } from 'react';
 import './Skills.scss';
 import { Element } from 'react-scroll';
-import AnimateSkill from './animate-skills/AnimateSkill';
+import TechBox from '../../components/project-display/card/techBox/TechBox';
+import useElementOnScreen from '../../util/useElementOnScreen';
 
 function Skills() {
+  const [data, setData] = useState(null);
+  const frontRef = useRef(null);
+  const backRef = useRef(null);
+  const otherRef = useRef(null);
+  useEffect(() => {
+    async function getData() {
+      const res_1 = await fetch('/files/other/skills.json');
+      const fetchedData = await res_1.json();
+      setData(fetchedData);
+    }
+    getData();
+  }, []);
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [containerRef] = useElementOnScreen({
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.7,
+  }, isVisible, setIsVisible, true);
+
+  useEffect(() => {
+    if (isVisible) {
+      frontRef.current.classList.add('show-left');
+      setTimeout(() => {
+        backRef.current.classList.add('show-left');
+      }, 100);
+      setTimeout(() => {
+        otherRef.current.classList.add('show-left');
+      }, 200);
+    }
+  }, [isVisible, containerRef]);
+
   return (
     <Element id="scroll-skills" name="scroll-skills">
-      <section className="skills-section">
-        <h1 className="skills-header">Skills</h1>
-        <div className="front">
+      <section className="skills-section" ref={containerRef}>
+        <h1 className="skills-header">Skills & Projects</h1>
+        <div className="front" ref={frontRef}>
           <p>Front end:</p>
+          <div className="tech-icons-container">
+            {data && data.frontEnd.map((tech, index) => (
+              <TechBox text={tech} tech={tech} color="white" key={`frontend-tech-${index}`} />
+            ))}
+          </div>
         </div>
-        <div className="back">
+        <div className="back" ref={backRef}>
           <p>Back end:</p>
+          <div className="tech-icons-container">
+            {data && data.backEnd.map((tech, index) => (
+              <TechBox text={tech} tech={tech} color="white" key={`backend-tech-${index}`} />
+            ))}
+          </div>
         </div>
-        <div className="other">
+        <div className="other" ref={otherRef}>
           <p>Other:</p>
+          <div className="tech-icons-container">
+            {data && data.other.map((tech, index) => (
+              <TechBox text={tech} tech={tech} color="white" key={`other-tech-${index}`} />
+            ))}
+          </div>
         </div>
       </section>
-      <AnimateSkill />
     </Element>
 
   );
--- src/sections/7-skills/animate-skills/AnimateSkill.jsx
+++ src/sections/7-skills/animate-skills/AnimateSkill.jsx
@@ -3,17 +3,11 @@ import './AnimateSkills.scss';
 import useElementOnScreen from '../../../util/useElementOnScreen';
 
 function AnimateSkill() {
-  const [isVisible, setIsVisible] = useState(true);
-  const [containerRef] = useElementOnScreen({
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.35,
-  }, isVisible, setIsVisible, true);
   return (
     <div ref={containerRef} className="skills-scene">
       {/* <img className="static-asset wolf" alt="Wolf" src="/images/skills/wolf-and-trees.webp" /> */}
-      <img className="static-asset forest-white" alt="Wolf" src="/images/skills/1w.png" />
-      <img className="static-asset bg" alt="Wolf" src="/images/skills/bg.png" />
+      {/* <img className="static-asset forest-white" alt="Wolf" src="/images/skills/1w.png" /> */}
+      {/* <img className="static-asset bg" alt="Wolf" src="/images/skills/bg.png" /> */}
       {/* <img className="static-asset forest-green" alt="Wolf" src="/images/skills/1g.png" /> */}
       {/* <img className="static-asset deer-male deer-couple" alt="Deer male" src="/images/animation-4/deer-male.webp" ref={deer_couple} />
       <img className="static-asset deer-female deer-couple" alt="Deer female" src="/images/animation-4/deer-female.webp" ref={deer_couple} /> */}
