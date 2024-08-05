2b2b544d2449ce73451f8ed3fccb93422227a828
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-08-05 16:03:40 +0200

    Test vercel fix

--- src/components/project-display/card/ProjectDescription.jsx
+++ src/components/project-display/card/ProjectDescription.jsx
@@ -7,6 +7,27 @@ import { useNavigate } from 'react-router-dom';
 function ProjectDescription({ projectData, color, caseStudyPath = '#' }) {
   const navigate = useNavigate();
   const [data, setData] = useState(null);
+  const caseStudyRef = useRef(null);
+
+  useEffect(() => {
+    const element = caseStudyRef.current;
+    function handleMouseDown(event) {
+      if (event.button === 1) {
+        event.preventDefault();
+        console.log('mm clicked');
+      }
+    }
+
+    if (element) {
+      element.addEventListener('mousedown', handleMouseDown);
+    }
+
+    return () => {
+      if (element) {
+        element.removeEventListener('mousedown', handleMouseDown);
+      }
+    };
+  }, []);
 
   useEffect(() => {
     setData(projectData);
@@ -21,7 +42,7 @@ function ProjectDescription({ projectData, color, caseStudyPath = '#' }) {
             {' '}
             For more information see this projects
             {' '}
-            <button className="case-study-link" onClick={() => navigate(caseStudyPath)} type="button">case study.</button>
+            <button className="case-study-link" onClick={() => navigate(caseStudyPath)} type="button" ref={caseStudyRef}>case study.</button>
           </>
         )}
 
