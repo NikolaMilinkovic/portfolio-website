afa2ea1a7cec9f1b8cd9c310859a48af7f92286c
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-08-07 00:59:36 +0200

    Test gtag issue

--- src/App.jsx
+++ src/App.jsx
@@ -17,25 +17,25 @@ function App() {
   const [showDropdown, setShowDropdown] = useState(true);
   const location = useLocation();
 
-  // Google Analytics
-  useEffect(() => {
-    // Set the page title based on the current path
-    const titleMap = {
-      '/': 'Home | Your Website',
-      '/case-study/battleship': 'Battleship Case Study | Your Website',
-      '/case-study/mc-schematic-manager': 'MC Schematic Manager | Your Website',
-      '/case-study/cli-data-structures': 'CLI Data Structures | Your Website',
-      '/case-study/portfolio-website': 'Portfolio Website Case Study | Your Website',
-    };
-    const title = titleMap[location.pathname] || 'Default Portfolio Website Pageview';
-    document.title = title;
+  // // Google Analytics
+  // useEffect(() => {
+  //   // Set the page title based on the current path
+  //   const titleMap = {
+  //     '/': 'Home | Your Website',
+  //     '/case-study/battleship': 'Battleship Case Study | Your Website',
+  //     '/case-study/mc-schematic-manager': 'MC Schematic Manager | Your Website',
+  //     '/case-study/cli-data-structures': 'CLI Data Structures | Your Website',
+  //     '/case-study/portfolio-website': 'Portfolio Website Case Study | Your Website',
+  //   };
+  //   const title = titleMap[location.pathname] || 'Default Portfolio Website Pageview';
+  //   document.title = title;
 
-    // Track the page view with gtag.js and include the title
-    window.gtag('config', 'G-8309RXRL30', {
-      page_path: location.pathname,
-      title,
-    });
-  }, [location]);
+  //   // Track the page view with gtag.js and include the title
+  //   window.gtag('config', 'G-8309RXRL30', {
+  //     page_path: location.pathname,
+  //     title,
+  //   });
+  // }, [location]);
 
   return (
     <NavbarProvider>
