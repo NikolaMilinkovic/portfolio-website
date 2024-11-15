b4a5950edf183b4b40ed8a4f094428f242980c27
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-11-15 12:49:26 +0100

    Update AiChat

--- src/util/getAiInfo.jsx
+++ src/util/getAiInfo.jsx
@@ -4,7 +4,7 @@ export function getAboutInfo() {
   const now = new Date();
   const currentDate = now.toDateString();
 
-  const aboutMeText = `name: Nikola Milinkovic, Web Developer, date of birth: 27.04.1997, current date for calculating the age is ${currentDate}, lives in Belgrade the capital city of Serbia. Not married.
+  const aboutMeText = `name: Nikola Milinkovic, Software Developer, date of birth: 27.04.1997, current date for calculating the age is ${currentDate}, lives in Belgrade the capital city of Serbia. Not married.
  Languages:
  - Serbian (Native)
  - English (Proficient)
@@ -19,6 +19,12 @@ export function getAboutInfo() {
 
  Social Links: LinkedIn - https://www.linkedin.com/in/nikola-milinkovic-2231451a6/ | Instagram - https://www.instagram.com/nikola__milinkovic/ | GitHub - https://github.com/NikolaMilinkovic | Email - nikolamilinkovic221@gmail.com. You can also contact me via the contact section on my webpage - https://nikola-portfolio-website.vercel.app/
 
+ My professional experience:
+
+ 2 years of working for Golux Technologies as a freelance Frontend Developer. Worked on multiple web apps, collaborated with design & backend teams to implement the most optimal solution.
+
+ 2 years of working for Game of Edication as a Game Designer & World Builder. Contributed to the success of multiple top performing projects through panning, design, testing. This includes projects such as "How to live inside an Axolotl", "TNT Tools" & "Summer Beach Bunker".
+
  Currently, I am freelancing and looking for full-time opportunities. For a full list of projects, you can check out the projects section or my GitHub profile as I keep it up to date.
 
  I enjoy traveling and exploring new places. I don't mind relocating for a great professional opportunity that I think will have great benefits for my career. When it comes to doing business I am open to freelance projects as well as full-time and part-time opportunities.
@@ -28,6 +34,7 @@ export function getAboutInfo() {
  To stay updated in the tech world I follow multiple tech channels such as Fireship, ThePrimeTime/ThePrimeagen, WebDevSimplified & Kevin Powell, and am subscribed to the DEV Digest newsletter.
 
  Some of the projects that I am most proud of are:
+ Infinity Boutique App - https://github.com/NikolaMilinkovic/infinity-ui
  Minecraft Schematic Manager - https://mc-schematic-manager.vercel.app/
  CLI-Data-Structures - https://nikolamilinkovic.github.io/CLI-data-structures/
  Battleship - https://nikolamilinkovic.github.io/battleship/
